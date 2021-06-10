7b8cce4523dbc5ecdeda4cf7413eaff5521b5a60
easyearn77 <58412156+easyearn77@users.noreply.github.com>
2021-06-10 23:28:20 +0900

    10_June//jieon correct code(2)

--- step/6월10일_큐,덱(1)_여주/큐2_여주.py
+++ step/6월10일_큐,덱(1)_여주/큐2_여주.py
@@ -1,11 +1,11 @@
 n = int(input())
-queue = []
+queue = [] #큐는 deque()사용하는게 편함 (시간 복잡도) // queue=deque([]) <전 문제랑 동일하게 import해 줘야함>
 count = 0
 
 for i in range(n):
-  order = input().strip()
-  if order.split()[0] == 'push':
-    queue.append(int(order.split()[1]))
+  order = input().strip() #order=input().split()
+  if order.split()[0] == 'push': #if order[0]=='push'
+    queue.append(int(order.split()[1])) #queue.append(order[1])
   elif order.split()[0] == 'pop':
     if len(queue) - count == 0:
       print(-1)
