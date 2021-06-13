a6c2422784f190289c3219ccba9df76b3699392a
yeojoo1224 <45957904+yeojoo1224@users.noreply.github.com>
2021-06-13 22:31:07 +0900

    여주생성

---
+++ step/6월13일_큐,덱(4)_여주/AC_여주.py
@@ -0,0 +1,27 @@
+t = int(input())
+for i in range(t):
+    c = input()
+    n = int(input())
+    arr = input().rstrip()[1:-1].split(",")
+
+    if n == 0:
+        arr = []
+
+    l, r, re = 0, 0, True
+
+    for com in c:
+        if com == "R":
+            re = not re
+        else:
+            if re is True:
+                l += 1
+            else:
+                r += 1
+    if r + l <= n:
+        answer = arr[l:n - r]
+        if re is True:
+            print("[" + ",".join(answer) + "]")
+        else:
+            print("[" + ",".join(answer[::-1]) + "]")
+    else:
+        print("error")
