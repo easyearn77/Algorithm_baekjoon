e40e0d1c657e3e01bd28e2a7fed098304a56b035
easyearn77 <58412156+easyearn77@users.noreply.github.com>
2021-07-01 17:53:20 +0900

    Add files via upload

---
+++ step/7월1일_동적계획법(3)_지언/1520_내리막 길.py
@@ -0,0 +1,26 @@
+import sys
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def dfs(x, y):
+    if x == m - 1 and y == n - 1:
+        return 1
+    if dp[x][y] == -1:
+        dp[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if matrix[nx][ny] < matrix[x][y]:
+                    dp[x][y] += dfs(nx, ny)
+    print(x,y,dp[x][y])
+    return dp[x][y]
+
+
+m, n = map(int, sys.stdin.readline().split())
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+dp = [[-1] * n for _ in range(m)]
+
+print(dfs(0, 0))
