537a850be39585eee3a5616ece2b84bc934e285f
yeojoo1224 <45957904+yeojoo1224@users.noreply.github.com>
2021-07-05 16:02:33 +0900

    뀨쀼

---
+++ step/7월4일_동적계획법2(6)_여주/동전1_여주.py
@@ -0,0 +1,12 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+coin = []
+dp = [0 for i in range(k + 1)]
+dp[0] = 1
+for i in range(n):
+    coin.append(int(sys.stdin.readline()))
+for i in coin:
+    for j in range(1, k + 1):
+        if j - i >= 0:
+            dp[j] += dp[j - i]
+print(dp[k])
