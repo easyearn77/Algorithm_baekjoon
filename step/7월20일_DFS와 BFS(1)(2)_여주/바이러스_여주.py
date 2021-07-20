29432779806bcafb5004afa0be0eeeed323fb754
yeojoo1224 <45957904+yeojoo1224@users.noreply.github.com>
2021-07-21 01:17:57 +0900

    여주생성/ 다시화이팅!!!

---
+++ step/7월20일_DFS와 BFS(1)(2)_여주/바이러스_여주.py
@@ -0,0 +1,17 @@
+import sys
+dic={}
+for i in range(int(sys.stdin.readline())):
+    dic[i+1] = set()
+for j in range(int(sys.stdin.readline())):
+    a, b = map(int,sys.stdin.readline().split())
+    dic[a].add(b)
+    dic[b].add(a)
+
+def dfs(start, dic):
+    for i in dic[start]:
+        if i not in visited:
+            visited.append(i)
+            dfs(i, dic)
+visited = []
+dfs(1, dic)
+print(len(visited)-1)
