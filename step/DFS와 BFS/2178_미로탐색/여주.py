28e388060b911443cd1bd30c5223495f0fe0a484
easyearn77 <58412156+easyearn77@users.noreply.github.com>
2021-08-13 19:45:37 +0900

    feedback_jieon
    나랑 코드 짜는 방향은 비슷했는데 bfs함수로 분리시키는 것이 달랐다. 
    함수로 분리시키는게 코드를 보기에 더 좋은 것 같다.

--- step/DFS와 BFS/2178_미로탐색/여주.py
+++ step/DFS와 BFS/2178_미로탐색/여주.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
 
-def bfs(x, y): #bfs 사용
+def bfs(x, y): #bfs 사용 (지언: 함수로 분리하는거 좋은 것 같음)
   queue = deque() #양방향 큐 사용
   queue.append((x, y))
   while queue:
@@ -23,7 +23,7 @@ n, m = map(int, sys.stdin.readline().split())
 graph = []
 
 for _ in range(n):
-  graph.append(list(map(int, sys.stdin.readline().strip()))) #strip 사용하여 양 옆 공백 제거
+  graph.append(list(map(int, sys.stdin.readline().strip()))) #strip 사용하여 양 옆 공백 제거 (지언: 오 이렇게 입력받으면 되는군!)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
