3aeecdbdcffd4030afad8775d3a95368e2c5eaca
easyearn77 <58412156+easyearn77@users.noreply.github.com>
2021-08-16 01:33:50 +0900

    Aug 15_Algorithm Study_jieon

---
+++ step/DFS와 BFS/1697_숨바꼭질/지언.py
@@ -0,0 +1,28 @@
+#최단거리와 비슷한 문제이므로 bfs를 사용한다.
+
+import sys
+from collections import deque
+MAX=100001 #max 설정 안하면 오류남.
+
+n,k=map(int,sys.stdin.readline().split())
+road=[0 for _ in range(MAX)]
+queue=deque()
+
+#초기 정점
+queue.append(n)
+road[n]=1
+
+def bfs():
+    while queue:
+        x=queue.popleft()
+        nx=[x-1,x+1,2*x]
+        for i in range(3):
+            tmp=nx[i]
+            if 0<=tmp<MAX and road[tmp]==0:
+                road[tmp]=road[x]+1
+                queue.append(tmp)
+                if tmp==k: #시간 단축을 위한 조건문
+                    return
+
+bfs()
+print(road[k]-1)
